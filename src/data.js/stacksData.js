425ad14970d1b9c669d32cee05967f0911d76883
wonder <samuel.nai@yahoo.com>
2023-03-15 23:53:32 +0000

    add stack data

---
+++ src/data.js/stacksData.js
@@ -0,0 +1,28 @@
+import js from './../assets/js.svg'
+import mongo from './../assets/mongo.svg'
+import react from './../assets/react.svg'
+import native from './../assets/native.svg'
+import git from './../assets/git.svg'
+
+export const stacks =[
+    {
+        name: "JavaScript",
+        img: js
+    },
+    {
+        name: "Mongo db",
+        img: mongo
+    },
+    {
+        name: "React Js",
+        img: react
+    },
+    {
+        name: "React Native",
+        img: native
+    },
+    {
+        name: "Git",
+        img: git
+    },
+]
