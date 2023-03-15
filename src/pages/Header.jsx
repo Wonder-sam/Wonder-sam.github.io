88e12003f33e761d5b1b6efeda5abe2bcb387c4f
wonder <samuel.nai@yahoo.com>
2023-03-15 23:56:46 +0000

    add top navigation bar

---
+++ src/pages/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+import mobileClasses from "./../styles/header/mobileHeader.module.css"
+
+export default function Header () {
+    return (
+        <div className={mobileClasses.container}>
+            <div className={[mobileClasses.header]}>
+                <ul className={[mobileClasses.tabs]}>
+                    <li><NavLink to="Home" className={({isActive})=>isActive? mobileClasses.activeClass: mobileClasses.inactiveClass}>_home</NavLink></li>
+                    <li><NavLink to="About" className={({isActive})=>isActive? mobileClasses.activeClass: mobileClasses.inactiveClass}>_about</NavLink></li>
+                    <li><NavLink to="Projects" className={({isActive})=>isActive? mobileClasses.activeClass: mobileClasses.inactiveClass}>_projects</NavLink></li>
+                    <li><NavLink to="Contact" className={({isActive})=>isActive? mobileClasses.activeClass: mobileClasses.inactiveContact}>CONTACT ME</NavLink></li>
+                </ul>
+            </div>
+            <Outlet />
+        </div>
+    )
+}
