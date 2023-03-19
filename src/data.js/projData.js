38c47252bba2e838b6d0d309df80743631b3043f
wonder <samuel.nai@yahoo.com>
2023-03-19 03:01:42 +0000

    add data for project section

---
+++ src/data.js/projData.js
@@ -0,0 +1,29 @@
+import proj1 from './../assets/pro1.png'
+import proj2 from './../assets/pro2.png'
+
+export const projects = [
+    {
+        title: "Real estate Template",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices lorem non feugiat egestas amet.",
+        stack: "Web development",
+        link: "www.gish.com",
+        github: "",
+        image: proj1
+    },
+    {
+        title: "Real estate Template",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices lorem non feugiat egestas amet.",
+        stack: "Web development",
+        link: "www.gish.com",
+        github: "",
+        image: proj2
+    },
+    {
+        title: "Real estate Template",
+        about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices lorem non feugiat egestas amet.",
+        stack: "Web development",
+        link: "www.gish.com",
+        github: "",
+        image: proj1
+    },
+]
