b3af75f87e709ad482343138c845f5d4622c23b8
wonder <samuel.nai@yahoo.com>
2023-03-19 04:54:52 +0000

    edit classnames to improve responsiveness

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,18 +7,18 @@ export default function AboutMe () {
     return (
         <div  className={`${mobileClasses.aboutContainer} ${wideClasses.aboutContainer}`}>
             <div className={`${mobileClasses.aboutMe} ${wideClasses.aboutMe}`}>
-                <p className={mobileClasses.aboutTitle}>About Me</p>
-                <div className={mobileClasses.underline}/>
-                <p className={mobileClasses.about}>I am a software engineer who loves anything relating to creating 
+                <p className={`${mobileClasses.aboutTitle} ${wideClasses.aboutTitle}`}>About Me</p>
+                <div className={`${mobileClasses.underline} ${wideClasses.underline}`}/>
+                <p className={`${mobileClasses.about} ${wideClasses.about}`}>I am a software engineer who loves anything relating to creating 
                     <span className={mobileClasses.aboutEmp}> amazing mobile and web applications.</span> 
                 </p>
-                <p className={mobileClasses.about}>Aside programming, I love to
+                <p className={`${mobileClasses.about} ${wideClasses.about}`}>Aside programming, I love to
                     <span className={mobileClasses.aboutEmp}> game, go on adventures and listening to music.</span> 
                 </p>
-                <p className={mobileClasses.about}>I am a software engineer who loves anything relating to creating 
+                <p className={`${mobileClasses.about} ${wideClasses.about}`}>I am a software engineer who loves anything relating to creating 
                     <span className={mobileClasses.aboutEmp}> amazing mobile and web applications.</span> 
                 </p>
-                <p className={mobileClasses.about}>Aside programming, I love to
+                <p className={`${mobileClasses.about} ${wideClasses.about}`}>Aside programming, I love to
                     <span className={mobileClasses.aboutEmp}> game, go on adventures and listening to music.</span> 
                 </p>
             </div>
