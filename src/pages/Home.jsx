76d077600ae0ca8d99c7b365d553a823c3600849
wonder <samuel.nai@yahoo.com>
2023-03-19 04:54:31 +0000

    edit classnames to improve responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import mobileClasses from './../styles/home/mobileHome.module.css'
+import wideClasses from './../styles/home/wideHome.module.css'
 import me from '../assets/sam1.png'
 import AboutMe from '../components/AboutMe'
 import Stack from '../components/Stack'
@@ -9,9 +10,9 @@ import ContactMe from '../components/Contact'
 export default function Home (){
     return(
         <>
-            <div className={mobileClasses.main}>
-                <div className={mobileClasses.leftSide}></div>
-                <div className={mobileClasses.introduction}>
+            <div className={`${mobileClasses.main} ${wideClasses.main}`}>
+                <div className={`${mobileClasses.leftSide} ${wideClasses.leftSide}`}></div>
+                <div className={`${mobileClasses.introduction} ${wideClasses.introduction}`}>
                     <p className={[mobileClasses.salute]}>Hello world, I am</p>
                     <p className={[mobileClasses.name]}>Samuel Sowah Nai</p>
                     <p className={[mobileClasses.work]}>I build <b>software solutions</b> for both mobile &  web.</p>
@@ -22,7 +23,7 @@ export default function Home (){
                         </button>
                     </div>
                 </div>
-                <div className={mobileClasses.imageContainer}>
+                <div className={`${mobileClasses.imageContainer} ${wideClasses.imageContainer}`}>
                     <img src={me} className={mobileClasses.image} alt="Samuel Sowah Nai"/>
                 </div>
             </div>
