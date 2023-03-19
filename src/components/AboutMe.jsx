674a19b6f76255dae889e3a5a4a3ac6d12bd865e
wonder <samuel.nai@yahoo.com>
2023-03-19 03:31:34 +0000

    implement responsiveness

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import mobileClasses from './../styles/about/mobileAbout.module.css'
+import wideClasses from './../styles/about/wideAbout.module.css'
 import won from './../assets/wonnHA.jpg'
 
 export default function AboutMe () {
     return (
-        <div  className={mobileClasses.aboutContainer}>
-            <div className={mobileClasses.aboutMe}>
+        <div  className={`${mobileClasses.aboutContainer} ${wideClasses.aboutContainer}`}>
+            <div className={`${mobileClasses.aboutMe} ${wideClasses.aboutMe}`}>
                 <p className={mobileClasses.aboutTitle}>About Me</p>
                 <div className={mobileClasses.underline}/>
                 <p className={mobileClasses.about}>I am a software engineer who loves anything relating to creating 
@@ -21,15 +22,15 @@ export default function AboutMe () {
                     <span className={mobileClasses.aboutEmp}> game, go on adventures and listening to music.</span> 
                 </p>
             </div>
-            <div className={mobileClasses.aboutImages}>
-                <div className={mobileClasses.imageDiv}>
+            <div className={`${mobileClasses.aboutImages} ${wideClasses.aboutImages}`}>
+                <div className={`${mobileClasses.imageDiv} ${wideClasses.imageDiv}`}>
                     <img src={won} className={mobileClasses.image} alt="game"/>
                 </div>
-                <div className={mobileClasses.imageDivs}>
-                    <div className={mobileClasses.imageDiv}>
+                <div className={`${mobileClasses.imageDivs} ${wideClasses.imageDivs}`}>
+                    <div className={`${mobileClasses.imageDiv} ${wideClasses.imageDiv}`}>
                         <img src={won} className={mobileClasses.image} alt="anime"/>
                     </div>
-                    <div className={mobileClasses.imageDiv}>
+                    <div className={`${mobileClasses.imageDiv} ${wideClasses.imageDiv}`}>
                         <img src={won} className={mobileClasses.image} alt="adventure"/>
                     </div>
                 </div>
